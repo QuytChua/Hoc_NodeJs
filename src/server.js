8231bc9c60630eaa2ef6059f777b704d41414c00
QuytChua <22110261@student.hcmute.edu.vn>
2025-12-05 09:54:17 +0700

    mvc 1

--- src/server.js
+++ src/server.js
@@ -1,28 +1,14 @@
 const express = require("express");
-const path = require("path");
 const app = express();
-
+const configViewEngine = require("./config/viewEngine");
+const webRoutes = require("./routes/web");
 require("dotenv").config();
 const hostname = process.env.HOST_NAME || "localhost";
 const port = process.env.PORT || 8080;
 
-app.get("/", (req, res) => {
-  res.send("Hello World!");
-});
+configViewEngine(app);
 
-app.get("/abc", (req, res) => {
-  res.send("checking abc route");
-});
-
-//config static files
-app.use(express.static(path.join(__dirname, "public")));
-// config template engine
-app.set("views", path.join(__dirname, "views"));
-app.set("view engine", "ejs");
-
-app.get("/hoi", (req, res) => {
-  res.render("sample.ejs");
-});
+app.use("/test", webRoutes);
 
 app.listen(port, hostname, () => {
   console.log(`Example app listening on port ${port}`);
